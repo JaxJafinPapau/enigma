17828b962334228d5cd12d5caea102849fa4803b
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-02-21 21:47:09 -0700

    adds attributes and test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -2,10 +2,16 @@ require './test/test_helper'
 
 class EnigmaTest < Minitest::Test
   def setup
-    @enigma = Enigma.new
+    @enigma = Enigma.new("hello world", "02715", "040895")
   end
 
   def test_enigma_exists
     assert_instance_of Enigma, @enigma
+  end
+
+  def test_enigma_has_attributes
+    assert_equal "hello world", @enigma.message
+    assert_equal "02715", @enigma.key
+    assert_equal "040895", @enigma.date
   end
 end
