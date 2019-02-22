a7322900f94c9895150e64b42b1b26ac3039a754
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-02-21 22:14:28 -0700

    adds date_getter and test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -7,4 +7,13 @@ class Enigma
     @key = "02715"
     @date = "040895"
   end
+
+  def encrypt(message, key, date)
+  end
+
+  def date_getter
+    DateTime.now.day.to_s.rjust(2, "0") +
+    DateTime.now.month.to_s.rjust(2, "0") +
+    DateTime.now.year.to_s
+  end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -16,11 +16,18 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_enigma_can_encrypt_message
+    x = Date.today
+    binding.pry
     expected = {
                 encryption: "keder ohulw",
                 key: "02715",
                 date: "040895"
               }
-    assert_equal expected, @enigma.encrypt()
+    assert_equal expected, @enigma.encrypt(@enigma.message, @enigma.key, @enigma.date)
+  end
+
+  def test_enigma_can_get_current_date
+    expected = DateTime.now.day.to_s.rjust(2, "0") + DateTime.now.month.to_s.rjust(2, "0") + DateTime.now.year.to_s
+    assert_equal expected, @enigma.date_getter
   end
 end
