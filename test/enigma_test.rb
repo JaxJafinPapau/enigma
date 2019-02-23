752e5730c510c587f077a796e691526ef5e3f2b5
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-02-22 22:19:10 -0700

    adds rotated_index test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -18,14 +18,14 @@ class EnigmaTest < Minitest::Test
     assert_equal 5, enigma.key.length
   end
 
-  # def test_enigma_can_encrypt_message
-  #   expected = {
-  #               encryption: "keder ohulw",
-  #               key: "02715",
-  #               date: "040895"
-  #             }
-  #   assert_equal expected, @enigma.encrypt(@enigma.message, @enigma.key, @enigma.date)
-  # end
+  def test_enigma_can_encrypt_message
+    expected = {
+                encryption: "keder ohulw",
+                key: "02715",
+                date: "040895"
+              }
+    assert_equal expected, @enigma.encrypt
+  end
 
   def test_enigma_can_get_current_date
     enigma = Enigma.new("hello world", "02715")
@@ -47,5 +47,13 @@ class EnigmaTest < Minitest::Test
 
   def test_enigma_can_generate_offset
     assert_equal "1025", @enigma.offset
+  end
+
+  def test_alpha_rotator_can_rotate_alphabet
+    assert_equal "x", @enigma.alpha_rotator(04)[0]
+  end
+
+  def test_enigma_can_find_rotated_index
+    assert_equal 0 @enigma.rotated_index("x")
   end
 end
