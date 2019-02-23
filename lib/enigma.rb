06183c71438cd9d52b20c2113c2294c109c02a77
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-02-22 19:40:13 -0700

    adds offset method. adds offsets to shift methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -28,18 +28,23 @@ class Enigma
   end
 
   def a_shift
-    @key[0..1].to_i
+    @key[0..1].to_i + offset[0].to_i
   end
 
   def b_shift
-    @key[1..2].to_i
+    @key[1..2].to_i + offset[1].to_i
   end
 
   def c_shift
-    @key[2..3].to_i
+    @key[2..3].to_i + offset[2].to_i
   end
 
   def d_shift
-    @key[3..4].to_i
+    @key[3..4].to_i + offset[3].to_i
+  end
+
+  def offset
+    full_num = @date.to_i ** 2
+    full_num.to_s[-4..-1]
   end
 end
