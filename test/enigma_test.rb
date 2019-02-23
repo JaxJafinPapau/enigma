9a3e80ace4b1ea6cbad2c098c1b1fb2cd16f2c53
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-02-22 19:39:41 -0700

    adds generate_offset test. alters test values for shifts to accommodate offsets

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -39,9 +39,13 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_enigma_can_get_shifts
-    assert_equal 02, @enigma.a_shift
+    assert_equal 03, @enigma.a_shift
     assert_equal 27, @enigma.b_shift
-    assert_equal 71, @enigma.c_shift
-    assert_equal 15, @enigma.d_shift
+    assert_equal 73, @enigma.c_shift
+    assert_equal 20, @enigma.d_shift
+  end
+
+  def test_enigma_can_generate_offset
+    assert_equal "1025", @enigma.offset
   end
 end
