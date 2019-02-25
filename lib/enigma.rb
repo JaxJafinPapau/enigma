7ce3bf5648008d5e739eade8a66e0ce69c320384
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-02-25 15:45:15 -0700

    refactors letter_looper to a module

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -25,32 +25,6 @@ class Enigma
     {decryption: decrypted_message, key: key, date: date}
   end
 
-  def letter_looper(message, direction)
-    encrypted_letters = []
-    message_letters = message.chars
-    symbols = message_letters.map.with_index do |character, index|
-      if @alphabet.include?(character) == false
-        [character, index]
-      end
-    end.compact
-    ((message.length / 4) + 1).times do
-      message_letters.shift(4).map.with_index do |character, index|
-      next if @alphabet.include?(character) == false
-      encrypted_letters << @alphabet[(rotated_index(character, index, direction))]
-      end
-    end
-    symbols.each do |sym_ind|
-      encrypted_letters.insert(sym_ind[1], sym_ind[0])
-    end
-    encrypted_letters.compact
-  end
-
-  def rotated_index(character, index, direction)
-    direction == "forward" ? x = 1 : x = -1
-    shifts = [a_shift * x, b_shift * x, c_shift * x, d_shift * x]
-    (@alphabet.index(character) + shifts[index]) % 27
-  end
-
   def date_getter
     @date = DateTime.now.strftime("%d%m%y")
   end
