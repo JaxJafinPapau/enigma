2cfe797464d8272a73b25ff8db82145d95d9fd7b
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-02-24 20:14:52 -0800

    comepletes encryption_menu

--- lib/interface.rb
+++ lib/interface.rb
@@ -1,2 +1,14 @@
 class Interface
+  def welcome
+    p "Welcome to Enigma. Please type 'exit' at any time to quit.\n" +
+      "Please ensure you press 'return' after any input.\n" +
+      "Enter (e) to encrypt or (d) to decrypt."
+  end
+
+  def encryption_menu
+    p "Encryption Menu:\n" +
+      "Enter a message to encrypt\n" +
+      "Enter 'D' to add a custom date\n" +
+      "Enter 'K' to add a custom key."
+  end
 end
