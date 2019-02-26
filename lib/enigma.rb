5ff5e6a739f34fe7361718b89ac3b57f7823483f
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-02-25 17:18:43 -0700

    refactors encrypt to accomodate symbol stripper and adder methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -17,17 +17,18 @@ class Enigma
     @key = key
     @date = date
     @alphabet = [*("a".."z")].join + " "
-    @message_letters = message.chars
     @symbols = []
   end
 
   def encrypt(message = @message, key = @key, date = @date)
-    encrypted_message = letter_looper(message, "forward").join
-    {encryption: encrypted_message, key: key, date: date}
+    naked_message = symbol_stripper(message.chars)
+    encrypted_message = letter_looper(naked_message, "forward")
+    output = symbol_adder(encrypted_message).join
+    {encryption: output, key: key, date: date}
   end
 
   def decrypt(message, key, date)
-    decrypted_message = letter_looper(message, "backward").join
+    decrypted_message = letter_looper("backward").join
     {decryption: decrypted_message, key: key, date: date}
   end
 
