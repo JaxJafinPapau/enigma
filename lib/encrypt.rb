ee8f89173c317525559960091deb308ce42a752e
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-02-26 16:12:39 -0700

    adds downcase to input

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -6,4 +6,6 @@ enigma = Enigma.new(message.read.downcase)
 key = enigma.key
 date = enigma.date
 output.write(enigma.encrypt[:encryption])
+output.close
+message.close
 puts "Created '#{ARGV[1]}' with the key #{key} and date #{date}"
