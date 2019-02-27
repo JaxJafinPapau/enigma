af160ed9ce4e689faa1d08523aacf497e6196618
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-02-26 19:43:48 -0700

    moves date test to key_date_gen

--- test/key_and_date_generator_test.rb
+++ test/key_and_date_generator_test.rb
@@ -5,4 +5,10 @@ class KeyAndDateGeneratorTest < Minitest::Test
     @enigma = Enigma.new("hello world.", "02715", "040895")
   end
 
+  def test_enigma_can_get_current_date
+      enigma = Enigma.new("hello world", "02715")
+      assert_equal 6, enigma.date_getter.length
+      assert_instance_of String, enigma.date_getter
+      assert_instance_of Integer, enigma.date_getter.to_i
+  end
 end
